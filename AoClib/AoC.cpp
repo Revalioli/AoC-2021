7fb38fb684738b314285112607e66b0c6748158e
Revalioli <emmanuel.montmasson@gmail.com>
2021-12-17 14:42:07 +0100

    Adds new split() implementation

--- AoClib/AoC.cpp
+++ AoClib/AoC.cpp
@@ -2,18 +2,36 @@
 
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 namespace AoC{
 
 	std::vector<std::string> split(const std::string &str, char delimiter) {
-		std::istringstream iss(str);
-		std::string buff;
+//		==== Old implementation with istringstream ===
+
+//		std::istringstream iss(str);
+//		std::string buff;
+//		std::vector<std::string> result;
+//
+//		while(getline(iss, buff, delimiter))
+//			result.push_back(buff);
+
+//		return result;
+
+
+//		=== Faster implementation in release mode ===
 		std::vector<std::string> result;
 
-		while(getline(iss, buff, delimiter))
-			result.push_back(buff);
+		auto stop = str.end();
 
-		return result;
+		for(auto pos = str.begin();; pos++){
+			auto start = pos;
+			pos = std::find(pos, stop, delimiter);
+			result.emplace_back(start, pos);
+			if(pos == stop)
+				return result;
+		}
+
 	}
 
 	std::vector<std::string> split(const std::string &&str, char delimiter) {
