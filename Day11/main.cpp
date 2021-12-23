64deab7db71de595b98ce925065ce670561d8875
Revalioli <emmanuel.montmasson@gmail.com>
2021-12-23 14:44:37 +0100

    Day 11 in c++

---
+++ Day11/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include "AoC.h"
+
+using namespace std;
+
+using timeUnit = std::chrono::microseconds;
+
+void day11_1(const string& input);
+void day11_2(const string& input);
+
+int main(int argc, char* argv[]) {
+	ifstream input_f("input.txt");
+	string input;
+	ostringstream ss;
+	ss << input_f.rdbuf();
+	input = ss.str();
+
+	// Specific day
+	if(argc > 1){
+		switch(stoi(argv[1])){
+			case 1:
+				day11_1(input);
+				break;
+			case 2:
+				day11_2(input);
+				break;
+			default:
+				cout << "Part number should be 1 or 2" << endl;
+		}
+	}
+	else{
+		// ===[ Time benchmarks ]===
+
+		int samples = 500;
+
+		// Part 1 and 2
+		cout << "=== Part 1" << endl;
+		AoC::timeBenchmark<timeUnit>(day11_1, input, samples, "microseconds");
+
+		cout << "=== Part 2" << endl;
+		AoC::timeBenchmark<timeUnit>(day11_2, input, samples, "microseconds");
+	}
+}
